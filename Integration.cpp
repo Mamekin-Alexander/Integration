3787c26660c81eefa8facaf48d7aaeb4a0b691b5
Alexander Mamekin <mamekin.al@gmail.com>
2019-11-20 20:46:28 +0300

    Trapeze_vs_rectangle added

--- Integration.cpp
+++ Integration.cpp
@@ -1,13 +1,14 @@
 ﻿#include <iostream>
 #include <iomanip>
 const double inf = 6.0;
-typedef double type;
-const type correct = sqrt(4 * atan(1));
+typedef double default_type;
+const default_type correct = sqrt(4 * atan(1));
 
-type rectangle_left(int n)
+
+default_type rectangle_left(int n)
 {
-	type x, res=0;
-	type dx = 2 * inf / n;
+	default_type x, res=0;
+	default_type dx = 2 * inf / n;
 	x = -inf;
 	for (int i = 1; i <= n; i++)
 	{
@@ -16,10 +17,10 @@ type rectangle_left(int n)
 	}
 	return res;
 }
-type rectangle_right(int n)
+default_type rectangle_right(int n)
 {
-	type x, res = 0;
-	type dx = 2 * inf / n;
+	default_type x, res = 0;
+	default_type dx = 2 * inf / n;
 	x = -inf+dx;
 	for (int i = 1; i <= n; i++)
 	{
@@ -28,10 +29,10 @@ type rectangle_right(int n)
 	}
 	return res;
 }
-type rectangle_mid(int n)
+default_type rectangle_mid(int n)
 {
-	type x, res = 0;
-	type dx = 2 * inf / n;
+	default_type x, res = 0;
+	default_type dx = 2 * inf / n;
 	x = -inf;
 	for (int i = 1; i <= n; i++)
 	{
@@ -45,25 +46,25 @@ void L_M_R_rect()
 	int split_number = 6;
 	std::cout << "--------------------------------------------------------------------" << '\n';
 	std::cout << "Left vs middle vs right rectangles" << '\n';
-	std::cout << "Split number: " << split_number<<", Way: from left to right, All types - double" << '\n';
+	std::cout << "Split number: " << split_number<<", Way: from left to right, All default_types - double" << '\n';
 	std::cout << "     Left rectangle   " << std::setprecision(14)<< rectangle_left(split_number);
 	std::cout << "   Difference:" << std::setprecision(11)<< abs(rectangle_left(split_number) - correct) << '\n';
 	std::cout << "     Middle rectangle " << std::setprecision(14) << rectangle_mid(split_number);
 	std::cout << "   Difference:" << std::setprecision(11) << abs(rectangle_mid(split_number) - correct) << '\n';
 	std::cout << "     Right rectangle  " << std::setprecision(14) << rectangle_right(split_number);
 	std::cout << "   Difference:" << std::setprecision(11) << abs(rectangle_right(split_number) - correct) << '\n';
-	std::cout << "     Real value       " << std::setprecision(14) <<correct<<'\n';
+	std::cout << "     Real value:      " << std::setprecision(14) <<correct<<'\n';
 	std::cout << '\n' << '\n';
 
 	split_number = 7;
-	std::cout << "Split number: " << split_number << ", Way: from left to right, All types - double" << '\n';
+	std::cout << "Split number: " << split_number << ", Way: from left to right, All default_types - double" << '\n';
 	std::cout << "     Left rectangle   " << std::setprecision(14) << rectangle_left(split_number);
 	std::cout << "   Difference:" << std::setprecision(11) << abs(rectangle_left(split_number) - correct) << '\n';
 	std::cout << "     Middle rectangle " << std::setprecision(14) << rectangle_mid(split_number);
 	std::cout << "   Difference:" << std::setprecision(11) << abs(rectangle_mid(split_number) - correct) << '\n';
 	std::cout << "     Right rectangle  " << std::setprecision(14) << rectangle_right(split_number);
 	std::cout << "   Difference:" << std::setprecision(11) << abs(rectangle_right(split_number) - correct) << '\n';
-	std::cout << "     Real value       " << std::setprecision(14) << correct << '\n';
+	std::cout << "     Real value:      " << std::setprecision(14) << correct << '\n';
 
 
 	std::cout << "Function is symmetrical, so there is no difference between right and left rectangles." << '\n';
@@ -77,22 +78,39 @@ void R_L_W()
 {
 
 }
-type trapeze(int n)
+template <typename T>
+T trapeze(int n, T a)
 {
-	type dx = 2 * inf / n;
+	T dx = 2 * inf / n;
+	T x = -inf;
+	T res = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		res = res + (exp(-x * x) + exp(-(x + dx)*(x + dx))) / 2 * dx;
+		x = x + dx;
+	}
+	return res;
 }
 void trapeze_vs_rectangle()
 {
-	const int number_of_splits = 10;
+	const int number_of_splits = 6;
 	std::cout << "--------------------------------------------------------------------" << '\n';
 	std::cout << "Trapeze_vs_rectangle" << '\n';
-	std::cout << "Number_of_splits: " << number_of_splits << " Way: from left to right "<<"All types- double" << '\n';
-	std::cout << "     Rectangle: " << std::setprecision(14)<<rectangle_mid(number_of_splits);
+	std::cout << "Number_of_splits: " << number_of_splits << " Way: from left to right "<<"All default_types- double" << '\n';
+	std::cout << "     Rectangle:   " << std::setprecision(14)<<rectangle_mid(number_of_splits);
 	std::cout << "     Difference  " << std::setprecision(10) << abs(rectangle_mid(number_of_splits) - correct) << '\n';
+	std::cout << "     Trapeze:     " << std::setprecision(14) << trapeze(number_of_splits, (default_type)5.0);
+	std::cout << "     Difference  " << std::setprecision(10) << abs(trapeze(number_of_splits, (default_type)5.0) - correct) << '\n';
+	std::cout << "     Real value:  " << std::setprecision(14) << correct << '\n';
+	std::cout << "We can see, that rectangle is better here." << '\n';
+	std::cout << "It's so, because first derivative takes values from -0.78 to 0.78, but the second from -2 to 0.89." << '\n';
+	std::cout << "So in rectangle we have better aproximation, that why the result is more accurate" << '\n';
+	std::cout << "--------------------------------------------------------------------" << '\n';
 }
 
 int main()
 {
 	std::cout << "Infinity is set at " << inf << '\n';
 	L_M_R_rect();
+	trapeze_vs_rectangle();
 }
